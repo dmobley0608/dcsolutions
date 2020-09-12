2fa1b3f514393eb7c7fcbd2bc1be6e2d1cceeeec
dmobley0608 <67826887+dmobley0608@users.noreply.github.com>
2020-09-12 09:20:34 -0400

    Updates

---
+++ precache-manifest.cdb1cdce25ba16320e4ea48a1798736b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "db267db89397515ff356441b703b41b2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "aa1b8f496c817dffaa3f",
+    "url": "/static/css/main.d1c0eec9.chunk.css"
+  },
+  {
+    "revision": "6f05db03fe4a35d03284",
+    "url": "/static/js/2.13d0f8b0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.13d0f8b0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aa1b8f496c817dffaa3f",
+    "url": "/static/js/main.96bc8b6b.chunk.js"
+  },
+  {
+    "revision": "f24b46201a6003ae2bc6",
+    "url": "/static/js/runtime-main.bd24917c.js"
+  },
+  {
+    "revision": "b13d0926743277f2d0767737dd003b22",
+    "url": "/static/media/wallpaper.b13d0926.jpg"
+  }
+]);

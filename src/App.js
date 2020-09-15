c724f7f247b4040e7d183dc582cee50f8f49e6ea
dmobley0608 <67826887+dmobley0608@users.noreply.github.com>
2020-09-15 15:44:11 -0400

    updated contact page

--- src/App.js
+++ src/App.js
@@ -3,15 +3,40 @@ import Background from './components/Background/Background';
 import Greeting from './components/Greeting/Greeting'
 import './App.css';
 import GridMenu from './components/GridMenu/GridMenu';
+import ContactUs from './components/ContactUs/ContactUs';
 
-function App() {
+
+
+class App extends React.Component {
+  constructor(){
+    super();
+    this.state = {
+      route: 'contactUs'
+    }
+  }
+  
+  onRouteChange = (route) => {
+    this.setState({
+      route: route
+    });
+  }
+
+  render(){  
+    const {route} = this.state;
+    
+  
   return (
     <div className="App">
-       <Background/>   
+      <Background/>   
       <Greeting /> 
-      <GridMenu/>   
+      <GridMenu onRouteChange={this.onRouteChange}/>
+      {route === 'contactUs'
+      ?<ContactUs onRouteChange={this.onRouteChange}/>
+      :<div></div>
+      }   
     </div>
   );
+  }
 }
 
 export default App;
